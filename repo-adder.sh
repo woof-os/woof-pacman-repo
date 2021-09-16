e45f5a203282add23a7cd834a20da4e2f814ded5
ACuteWoof <sutharshanvithushan@gmail.com>
2021-09-16 09:49:02 +0530

    Modify repo-adder.sh

--- repo-adder.sh
+++ repo-adder.sh
@@ -5,11 +5,3 @@ rm ./woof-pacman-repo.files
 mv ./woof-pacman-repo.db.tar.gz woof-pacman-repo.db
 mv ./woof-pacman-repo.files.tar.gz woof-pacman-repo.files
 
-cd ./any
-repo-add ./woof-pacman-repo.db.tar.gz *.pkg.tar.zst
-rm ./woof-pacman-repo.db
-rm ./woof-pacman-repo.files
-mv ./woof-pacman-repo.db.tar.gz woof-pacman-repo.db
-mv ./woof-pacman-repo.files.tar.gz woof-pacman-repo.files
-
-
